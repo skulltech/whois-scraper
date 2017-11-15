5638dcbe7de72962e7600f0aa6e8a21995466422
Sumit Ghosh <sumit.ghosh32@gmail.com>
2017-11-15 09:28:40 +0530

    Add code to download the email image file

--- whois-scraper.py
+++ whois-scraper.py
@@ -2,6 +2,9 @@ from lxml import html
 from PIL import Image
 import requests
 import urllib.request
+import csv
+from io import BytesIO
+import time
 
 def enlarge_image(image_file):
 	image = Image.open(image_file)
@@ -29,8 +32,6 @@ def fix_emails(whois_data, image_urls):
 	return whois_data
 
 def scrape_whois(domain):
-	domain = 'speedtest.net'
-
 	page = requests.get('http://www.whois.com/whois/{}'.format(domain))
 	tree = html.fromstring(page.content)
 
@@ -38,3 +39,22 @@ def scrape_whois(domain):
 	registrar_images = list(map(lambda x: 'http://www.whois.com' + x, tree.xpath('//*[@id="registrarData"]/img/@src')))
 	registry_data = tree.xpath('//*[@id="registryData"]/text()')
 	registry_images = list(map(lambda x: 'http://www.whois.com' + x, tree.xpath('//*[@id="registryData"]/img/@src')))
+
+	for x, n in enumerate(registrar_images):
+		im = Image.open(BytesIO(requests.get(n).content))
+		im.save('{domain}-{index}.png'.format(domain=domain, index=x))
+	
+	return [registry_data, registrar_data]
+
+file_name = 'Book1.csv'
+
+
+with open(file_name, newline='') as input_csv:
+    csv_reader = csv.reader(input_csv)
+
+    with open('whois_data.csv', mode='w', newline='') as output_csv:
+        csv_writer = csv.writer(output_csv)
+        for row in csv_reader:
+            ret = scrape_whois(row[0])
+            csv_writer.writerow([row[0], '|'.join(ret[0]), '|'.join(ret[1])])
+            time.sleep(2)
